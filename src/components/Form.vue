51e8e644ee2dab6857fb90f59df82b7c4764c8c8
ad-boop <AF908@live.mdx.ac.uk>
2023-03-29 14:06:38 +0400

    added the checkout form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,18 +1,77 @@
 <template>
-  <div>
-    <h2>Checkout</h2>
-    <h3>Added products</h3>
-    <div v-for='(product,index) in cart' :key="index">
-        {{product.id}}
-        <button @click="cart.splice(index,1)">remove</button>
-    </div>
-    <p>
-      <strong>Name </strong>
-      <input v-model="name" />
-      <strong> Address </strong>
-      <input v-model="address" /> <button>Submit</button>
-    </p>
-  </div>
+  <!-- ------------------------CHECKOUT SECTION--------------------------------- -->
+        <div id="checkout-main-div">
+            
+          <!-- ------------- CART SECTION------------------------- -->
+          <div class="cart-div" id="cart-div">
+            <h1>Your Cart</h1>
+            <!-- CART ITEMS display -->
+            <div class="cart-items-div" id="cart-items-div">
+              <div class="cart-product-box" v-for="cartItem in cart" :key="cartItem.id">
+                <!-- cart items details -->
+                <h3 id="subject">{{cartItem.subject}}</h3>
+                <p id="location">Location: {{cartItem.location}}</p>
+                <p id="price">Price: ${{cartItem.price}}</p>
+
+                <!-- remove from cart button -->
+                <div id="remove-button-div">
+                  <button id="remove-btn" v-on:click="removeFromCart(cartItem)">
+                    Remove
+                  </button>
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+          <!-- If all products are removed from cart, button is displayed to take back to the products page -->
+          <div class="main-back-to-products">
+                <button v-on:click = "showCheckoutPage()" class = "goBackBtn">
+                    <span  class="fas fa-arrow-left">Back To Lesson Products</span>
+                </button>
+            </div>
+
+          <!-- ------------- CHECKOUT FORM SECTION------------------------- -->
+          <div class="checkout" id="checkout-div">
+            <h1>Checkout</h1>
+            <div class="checkout-form-div">
+
+                <!-- checkout form -->
+              <form action="" id="">
+                <label for="nameUser">Name:</label>
+                <input
+                  type="text"
+                  name="nameUser"
+                  id="nameUser"
+                  v-model="name"
+                  v-on:keypress="isLetter($event)"
+                />
+                <label for="phoneNumber">Phone:</label>
+                <input
+                  type="tel"
+                  name="phoneNumber"
+                  id="phoneNumber"
+                  v-model="phone_number"
+                  v-on:keypress="isNumber($event)"
+                />
+              </form>
+
+              <!-- checkout button -->
+              <div class="checkout-btn-div" id="checkout-btn-div">
+                <!-- if form not filled button disabled -->
+                <button id="checkout-button" v-if="name == '' || phone_number == ''" disabled="disabled">
+                  Checkout
+                </button>
+                <!-- if form is filled -->
+                <button id="checkout-button" v-else v-on:click="submitCheckout()" >
+                  Checkout
+                </button>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
 </template>
 
 <script>
@@ -22,14 +81,65 @@ export default {
   data() {
     return {
       name: "",
-      address: "",
+      phone_number: "",
     };
   },
   methods :{
-    removeproduct(product)
+    removeFromCart(lesson)
     {
-      this.$emit('remproduct',product)
-    }
+      this.$emit('removeLesson',lesson)
+    },
+    isLetter(e){
+            //for every character input
+            let char = String.fromCharCode(e.keyCode); 
+
+            //if the character is an alphabet
+            if(/^[A-Za-z]+$/.test(char)){
+                return true;
+            } 
+            else{
+                alert("Only letters for the name");
+                this.checkout.error_message="Only letters for the name"
+            } 
+          },
+
+
+          //function to check if the user input for phone has only numbers
+            isNumber(e){
+
+                //matching the user input with the character code of 0-9
+                if(e.charCode >= 48 && e.charCode <= 57 ){
+                    return true;
+                }
+                else{
+                    alert("Only numbers for the phone number");
+                    this.checkout.error_message="Only numbers for the phone number"
+                } 
+            },
+
+
+            //   checkout function for the checkout page
+          submitCheckout() {
+
+            // if there are items in the cart
+            if (this.cart.length > 0 && this.checkout.error_message=="") {
+              alert("Order Submitted!");
+              location.reload();
+            } 
+            // if there are no items in the cart
+            else if (this.cart.length == 0) {
+              alert("No Lessons in the Cart");
+            } 
+            // if validation showing incorrect
+            else if(this.checkout.error_message != null){
+              console.log(this.checkout.error_message)
+              alert("Fill the form properly")
+              this.checkout.error_message=""
+              this.checkout.name="";
+              this.checkout.phone_number="";
+            }
+           
+          },
 
   }
 };
