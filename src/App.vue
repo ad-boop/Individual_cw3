e1dbfc60aeca894830196f6b2dc6ccb309befe2a
ad-boop <AF908@live.mdx.ac.uk>
2023-03-29 13:37:04 +0400

    made changes

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>{{ sitename }}</h1>
+      <button @click="showCheckout">
+        {{ this.cart.length }}
+        Checkout
+      </button>
+    </header>
+    <main>
+     <product-list @addProduct="addToCart"></product-list>
+     
+      <checkout :cart="cart"></checkout>
+    </main>
+  </div>
+</template>
+
+<script>
+import productList from "./components/ProductList.vue";
+import checkout from "./components/Form.vue";
+
+export default {
+  name: "App",
+  components: {
+    productList,
+    checkout,
+  },
+  data() {
+    return {
+      sitename: "Vue.js Pet Depot",
+      cart: [],
+    };
+  },
+  methods: {
+    showCheckout() {},
+    addToCart(product) {
+     console.log("addProduct event received by the root component.");
+      this.cart.push(product);
+    },
+    
+  },
+};
+</script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div v-for="product in products" :key="product.id">
+      <h2>{{product.title}}</h2>
+      <figure>
+        <img v-bind:src="product.image" />
+      </figure>
+      <p v-html="product.description"></p>
+      <p>Price: {{ product.price }}</p>
+      <p>Available stock: {{ product.availableInventory }}</p>
+      <button @click="add(product)">Add to cart</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductList",
+  data() {
+    return {
+      products: [
+        {
+          id: 1001,
+          title: "Cat Food, 25lb bag",
+          description:
+            "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+          price: 20.0,
+          image: "product-fullsize.png",
+          availableInventory: 10,
+          rating: 2,
+        },
+        {
+          id: 1002,
+          title: "Yarn",
+          description:
+            "Yarn your cat can play with for a very <strong>long</strong> time!",
+          price: 2.99,
+          image: "yarn.jpg",
+          availableInventory: 7,
+          rating: 3,
+        },
+      ],
+    };
+  },
+  methods: {
+    add(product) {
+      console.log("added product", product.id);
+      this.$emit('addProduct',product);
+    },
+  },
+};
+</script>
