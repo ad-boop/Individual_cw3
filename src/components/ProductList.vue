30dcfaca909e37e4c49efd35c374f6c0036aedb1
ad-boop <AF908@live.mdx.ac.uk>
2023-03-31 15:39:49 +0400

    sort and search working

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -3,10 +3,66 @@
   <!---------------------------------- Lessons display SECTION---------------------------------- -->
         <div>
           <link rel="stylesheet" type="text/css" href="style.css">
+          <div id="search-div">
+
+            <!-- SEARCH BAR INPUT FIELD -->
+            <div class="search-bar">
+            <div class="search">
+              <input
+                type="text"
+                class="searchTerm"
+                placeholder="What are you looking for?"
+                v-model="searchInput"
+              />
+              <button type="submit" class="searchButton">
+                <i class="fa fa-search"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+
+
+        <!------------------------- SORT DIV------------------------------------------- -->
+        <div id="sortandbuttondiv">
+          
+            <div class="sort-div">
+
+                <!-- sort by the category-->
+                <div class="sort-by-div" style="text-align: center;">
+                    <h4>SORT BY:</h4>
+
+                    <!-- DROP DOWN FOR CATEGORY -->
+                    <select name="sort-by" id="sort-by" v-model="sort.sortType" style="align-items: center; margin: auto; text-align: center;">
+                        <option value="">No Sort By</option>
+                        <!-- looping all the sort categories -->
+                        <option v-for="(sortBy, key) in sort.type" v-bind:value="sortBy" style="text-transform: capitalize;" :key="key">{{key}}</option>
+                    </select>
+                </div>
+
+                <!-- sort order -->
+                <div class="sort-order-div">
+                    <h4>SORT ORDER:</h4>
+
+                    <!-- DROP DOWN FOR ORDER -->
+                    <!-- <select name="sort-order" id="sort-order" v-model="sort.sortOrder">
+                        <option value="">No Order</option> -->
+                        <!-- looping all the sort order -->
+                        <!-- <option v-for="(sortOrder, key) in sort.order" v-bind:value="sortOrder" style="text-transform: capitalize;">{{key}}</option>
+                    </select> -->
+
+                    <div v-for="(sortOrder, key) in sort.order" style="align-items: center; text-align: center;" :key="key">
+                      <span class="radio" style="display:inline;">
+                        <input type="radio" id="dec" v-bind:value="sortOrder" v-model="sort.sortOrder" style="display:inline; width: auto;">
+                        <label for="dec" class="radioButton" style="font-size:13px; display:inline;">{{key}}</label>
+                      </span>
+                    </div>
+                </div>
+          </div>
+        </div>
           <div class="lessons-div" id="lessons-div">
           <div class="school_lessons">
             <!-- lesson product boxess -->
-            <div id="lesson-box" v-for="lesson in lessons" :key="lesson.id">
+            <div id="lesson-box" v-for="lesson in filteredLessonList" :key="lesson.id">
               <div id="row">
                 <!-- lesson image -->
                 <div class="column left lesson-icon">
@@ -83,6 +139,24 @@ export default {
   props:['lessons'],
   data() {
     return {
+      sort: { //sort data
+
+            type: { //sort categories
+              location: "location",
+              subject: "subject",
+              price: "price",
+              availability: "space",
+            },
+
+            order: { //sort order
+              ascending: "1",
+              descending: "-1",
+            },
+
+            sortType: "", //data for sort type selected
+            sortOrder: "", //data for sort order selected
+          },
+            searchInput: "", //data from the search bar
     };
   },
   methods: {
@@ -104,10 +178,73 @@ export default {
           canAddToCart(lesson) {
             return lesson.space > 0;
           },
-
-          
-
   },
+  computed:{
+     //   sort lessons function
+          sortedLessons: function () {
+
+            let type = this.sort.sortType;
+            let order = this.sort.sortOrder;
+            console.log(type, order);
+
+            let lessonArray = this.lessons.slice(0);
+            function compare(a, b) {
+
+                // ascending
+              if (order == 1) {
+                if (type == "location") {
+                  if (a.location > b.location) return 1;
+                  if (a.location < b.location) return -1;
+                } 
+                else if (type == "subject") {
+                  if (a.subject > b.subject) return 1;
+                  if (a.subject < b.subject) return -1;
+                } 
+                else if (type == "price") {
+                  if (a.price > b.price) return 1;
+                  if (a.price < b.price) return -1;
+                } 
+                else {
+                  if (a.space > b.space) return 1;
+                  if (a.space < b.space) return -1;
+                }
+
+                // descending
+              } else if (order == -1) {
+                if (type == "location") {
+                  if (a.location < b.location) return 1;
+                  if (a.location > b.location) return -1;
+                } 
+                else if (type == "subject") {
+                  if (a.subject < b.subject) return 1;
+                  if (a.subject > b.subject) return -1;
+                } 
+                else if (type == "price") {
+                  if (a.price < b.price) return 1;
+                  if (a.price > b.price) return -1;
+                } 
+                else {
+                  if (a.space < b.space) return 1;
+                  if (a.space > b.space) return -1;
+                }
+              }
+              return 0;
+            }
+            return lessonArray.sort(compare);
+          },
+
+
+        //   search function
+          filteredLessonList: function () {
+
+            return this.sortedLessons.filter((post) => {
+              return (
+                post.subject.toLowerCase().includes(this.searchInput.toLowerCase()) ||
+                post.location.toLowerCase().includes(this.searchInput.toLowerCase())
+              );
+            });
+          },
+  }
 };
 </script>
 
